ebbe7db210fcc66324811cfc3015fa3063fc3bfc
teodord25 <djuric.teodor25@gmail.com>
2025-01-28 00:32:27 +0100

    custom sdk

--- flake.nix
+++ flake.nix
@@ -1,30 +1,37 @@
 {
-  description = "Flutter Android App with Nix and Neovim";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            android_sdk.accept_license = true;
-            allowUnfree = true;  # Allow unfree packages
-          };
+description = "Flutter 3.13.x";
+inputs = {
+  nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+  flake-utils.url = "github:numtide/flake-utils";
+};
+outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          android_sdk.accept_license = true;
+          allowUnfree = true;
         };
-        flutter = pkgs.flutter;
-        androidSdk = pkgs.androidsdk;
-        dart = pkgs.dart;
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [ flutter androidSdk dart ];
-          shellHook = ''
-            export ANDROID_HOME=${androidSdk}
-          '';
+      };
+      buildToolsVersion = "34.0.0";
+      # custom sdk for lighter build
+      androidComposition = pkgs.androidenv.composeAndroidPackages {
+        buildToolsVersions = [ buildToolsVersion "34.0.0" ];
+        platformVersions = [ "34" ];
+        abiVersions = [ "arm64-v8a" ];
+      };
+      androidSdk = androidComposition.androidsdk;
+    in
+    {
+      devShell =
+        with pkgs; mkShell rec {
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          buildInputs = [
+            flutter
+            androidSdk # the customized SDK made above
+            jdk17
+          ];
         };
-      });
+    });
 }
